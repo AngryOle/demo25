22e8c428143219fe8504c2a953b4fbe8a4712b13
AngryOle <olemhhaetta@hotmail.no>
2025-01-19 21:43:27 +0100

    Code for /tmp/poem/ and /tmp/quote/
    Added code in script.mjs that adds two functions. First function "getPoem" returns a poem. Second function "getRandomQuote" returns one out of five random quotes when requested by server.

--- script.mjs
+++ script.mjs
@@ -13,6 +13,33 @@ function getRoot(req, res, next) {
 
 server.get("/", getRoot);
 
+function getPoem(req, res) {
+    const poem = `
+        Roses are red,
+        Violets are blue,
+        These poems are so cheesy,
+        and I think you're grate too.
+    `;
+    res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
+}
+
+server.get("/tmp/poem", getPoem);
+
+function getRandomQuote(req, res) {
+    const quotes = [
+        "Now and then we had a hope that if we lived and were good, God would permit us to be pirates. - Mark Twain",
+        "It's more fun to be a pirate than to join the navy. - Steve Jobs",
+        "When a pirate grows rich enough, they make him a prince. - George R R Martin",
+        "Merchant and pirate were for a long period one and the same person. Even today mercantile morality is really nothing but a refinement of piratical morality. - Friedrich Nietzsche",
+        "There is more treasure in books than in all the pirate's loot on Treasure Island. - Walt Disney"
+    ];
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(HTTP_CODES.SUCCESS.OK).send(randomQuote).end();
+}
+
+server.get("/tmp/quote", getRandomQuote);
+
+
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
